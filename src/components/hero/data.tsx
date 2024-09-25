72f6e009327b6b54c8c35d18760acf6bd30b8854
Kevin Cullen <157893369+kpcullen@users.noreply.github.com>
2024-09-25 15:26:11 +0200

    initial commit

--- src/components/hero/data.tsx
+++ src/components/hero/data.tsx
@@ -9,21 +9,22 @@ export const title = {
 };
 
 export const profileCard = {
-  title: "Jacob Pixler",
+  title: "Kevin Cullen",
   subTitle: "Fullstack Software Developer",
-  body: `Passionate about building scalable, user-friendly web applications. Always exploring new technologies and best practices to improve development processes. And I like cats.`,
+  body: `Code-hungry, experience-seeking junior full-stack developer with a passion for solving challenging problems with apps that solve challenging real-world problems.`,
   socialUrls: [
     {
-      url: "https://www.linkedin.com/in/jacob-pixler-094348219/",
+      url: "https://www.linkedin.com/in/kevinpatrickcullen/",
       icon: <FaLinkedinIn size={20} />,
     },
     {
-      url: "https://github.com/yoyocharlie",
+      url: "https://github.com/kpcullen",
       icon: <IoLogoGithub size={20} />,
     },
   ],
   tooltip: {
-    imagePath: "https://avatars.githubusercontent.com/u/106707326?v=4",
+    imagePath:
+      "https://avatars.githubusercontent.com/u/157893369?s=400&u=1f2b2f6415a6962d73ab8e51439b30af9a4d5874&v=4",
     title: "Hey...",
     subTitle: "That's me!",
   },
